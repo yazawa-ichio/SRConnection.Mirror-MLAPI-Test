703fbc71d26075d9294865c00a4eb22f30a369b4
yazawa-ichio <yazawa922473@gmail.com>
2020-10-09 00:39:34 +0900

    MLAPIのサンプルをSRConnectionのトランスポートにしたものを作成

---
+++ Unity/Assets/SRConnection.MLAPI.Examples/Chat.cs
@@ -0,0 +1,40 @@
+﻿using Boo.Lang;
+using MLAPI;
+using MLAPI.NetworkedVar;
+using MLAPI.NetworkedVar.Collections;
+using UnityEngine;
+
+namespace SRConnection.Examples
+{
+
+	public class Chat : NetworkedBehaviour
+	{
+		private NetworkedList<string> ChatMessages = new NetworkedList<string>(new NetworkedVarSettings()
+		{
+			ReadPermission = NetworkedVarPermission.Everyone,
+			WritePermission = NetworkedVarPermission.Everyone,
+			SendTickrate = 5
+		}, new List<string>());
+
+		private string textField = "";
+
+		private void OnGUI()
+		{
+			if (IsClient || IsHost)
+			{
+				textField = GUILayout.TextField(textField, GUILayout.Width(200));
+
+				if (GUILayout.Button("Send") && !string.IsNullOrWhiteSpace(textField))
+				{
+					ChatMessages.Add(textField);
+					textField = "";
+				}
+
+				for (int i = ChatMessages.Count - 1; i >= 0; i--)
+				{
+					GUILayout.Label(ChatMessages[i]);
+				}
+			}
+		}
+	}
+}
---
+++ Unity/Assets/SRConnection.MLAPI.Examples/ChatStart.cs
@@ -0,0 +1,27 @@
+﻿using MLAPI;
+using UnityEngine;
+
+namespace SRConnection.Examples
+{
+	public class ChatStart : MonoBehaviour
+	{
+		[SerializeField]
+		GameObject m_Prefab = null;
+
+		private void OnGUI()
+		{
+			if (!NetworkingManager.Singleton.IsClient && !NetworkingManager.Singleton.IsHost)
+			{
+				if (GUILayout.Button("StartHost"))
+				{
+					NetworkingManager.Singleton.StartHost();
+					GameObject.Instantiate(m_Prefab).GetComponent<NetworkedObject>().Spawn();
+				}
+				if (GUILayout.Button("StartClient"))
+				{
+					NetworkingManager.Singleton.StartClient();
+				}
+			}
+		}
+	}
+}
